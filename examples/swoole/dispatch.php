fcec04b35fc69f5f4a3b5a3365e7f33a5a623cdf
matyhtf <mikan.tenny@gmail.com>
2017-05-19 19:43:43 +0800

    add swoole example file.

---
+++ examples/swoole/dispatch.php
@@ -0,0 +1,18 @@
+<?php
+$serv = new swoole_server("0.0.0.0", 9502);
+//$serv = new swoole_server("0.0.0.0", 9502);
+$serv->set(array(
+    'worker_num' => 4,
+    'dispatch_func' => 'my_dispatch_function',
+));
+
+$serv->on('connect', function (swoole_server $serv, $fd, $from_id) {
+	//echo "connect\n";;
+});
+
+$serv->on('receive', function (swoole_server $serv, $fd, $from_id, $data) {
+  var_dump($data, $fd, $serv->worker_id);
+});
+
+
+$serv->start();
