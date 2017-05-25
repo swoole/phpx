8e4b30cf65cfa6dfc8e54cd22fdffbc254ef3a25
matyhtf <mikan.tenny@gmail.com>
2017-05-25 11:37:53 +0800

    added test extension.

---
+++ examples/test/test.php
@@ -0,0 +1,23 @@
+<?php
+var_dump(cpp_hello_world2(1234, "test"));
+echo "hello world\n";
+
+cpp_ext_test(1234, 42.33, "jjj");
+cpp_ext_test2(["abc"]);
+myClass::test();
+
+
+ReflectionClass::export("CppClass");
+// exit;
+//var_dump($r);
+
+CppClass::test("xxx");
+
+$obj = new \CppClass;
+var_dump($obj->name);
+var_dump($obj->test2());
+echo "Countable interface: ".count($obj)."\n";
+
+$obj2 = new \CppClass;
+var_dump($obj2->name);
+var_dump($obj2->test2());
