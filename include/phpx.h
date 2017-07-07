d32d2ccc1c44656e46285733d0db112fcca0e8bb
matyhtf <mikan.tenny@gmail.com>
2017-07-07 15:07:37 +0800

    修复错误的代码

--- include/phpx.h
+++ include/phpx.h
@@ -1173,7 +1173,7 @@ static Variant global(const char *name)
     zend_string *key = zend_string_init(name, strlen(name), 0);
     zend_is_auto_global(key);
     zval *var = zend_hash_find_ind(&EG(symbol_table), key);
-    zend_string_free(var);
+    zend_string_free(key);
     if (!var)
     {
         return false;
