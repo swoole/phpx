7fc3937a656a5ffe13379eb72455e47960fb90bd
owenliang <120848369@qq.com>
2017-07-20 17:01:46 +0800

    support ini declare for extension

--- include/phpx.h
+++ include/phpx.h
@@ -1642,6 +1642,9 @@ extern int extension_after_request(int type, int module_number);
 
 class Extension
 {
+    friend int extension_startup(int type, int module_number);
+    friend int extension_shutdown(int type, int module_number);
+
 protected:
     zend_module_entry module =
     {
@@ -1658,6 +1661,16 @@ protected:
     NULL, //version
     STANDARD_MODULE_PROPERTIES,
     };
+
+    // INI
+    struct IniEntry {
+        std::string name;
+        std::string default_value;
+        int modifiable;
+    };
+
+    void registerIniEntries(int module_number);
+    void unregisterIniEntries(int module_number);
 
 public:
 
@@ -1700,6 +1713,16 @@ public:
         this->body = body;
     }
 
+    // modifiable can be one of these:PHP_INI_SYSTEM/PHP_INI_PERDIR/PHP_INI_USER/PHP_INI_ALL
+    void addIniEntry(const char* name, const char* default_value = "", int modifiable = PHP_INI_ALL)
+    {
+        IniEntry entry;
+        entry.name = name;
+        entry.default_value = default_value;
+        entry.modifiable = modifiable;
+        ini_entries.push_back(entry);
+    }
+
     string name;
     string version;
     bool started = false;
@@ -1717,6 +1740,8 @@ protected:
     int function_array_size = 0;
     int deps_count = 0;
     int deps_array_size = 0;
+
+    std::vector<IniEntry> ini_entries;
 };
 
 extern unordered_map<string, Extension*> _name_to_extension;
