bea9cc7d6e0315d9565f3c9c0f0465a6e68a437c
韩天峰-Rango <mikan.tenny@gmail.com>
2021-03-11 07:59:13 +0800

    Update gen_stub.php

--- script/gen_stub.php
+++ script/gen_stub.php
@@ -1,6 +1,28 @@
 #!/usr/bin/env php
 <?php declare(strict_types=1);
 
+// copy from https://github.com/php/php-src/blob/master/build/gen_stub.php
+
+/*
+   +----------------------------------------------------------------------+
+   | Zend Engine                                                          |
+   +----------------------------------------------------------------------+
+   | Copyright (c) Zend Technologies Ltd. (http://www.zend.com)           |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.00 of the Zend license,     |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.zend.com/license/2_00.txt.                                |
+   | If you did not receive a copy of the Zend license and are unable to  |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@zend.com so we can mail you a copy immediately.              |
+   +----------------------------------------------------------------------+
+   | Authors: Andi Gutmans <andi@php.net>                                 |
+   |          Zeev Suraski <zeev@php.net>                                 |
+   |          Dmitry Stogov <dmitry@php.net>                              |
+   +----------------------------------------------------------------------+
+*/
+
 require_once dirname(__DIR__).'/console/vendor/autoload.php';
 
 use PhpParser\Comment\Doc as DocComment;
