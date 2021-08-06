4907d0cb8b914dc14de5c60c45cc96463a8b7636
matyhtf <mikan.tenny@gmail.com>
2021-08-06 16:11:12 +0800

    Optimize tests

--- tests/src/exec.cpp
+++ tests/src/exec.cpp
@@ -2,8 +2,7 @@
 
 using namespace php;
 
-TEST(variant, toCString) {
-    std::string s1("hello world");
-    Variant s2("hello world");
-    ASSERT_STREQ(s1.c_str(), s2.toCString());
+TEST(exec, func) {
+    auto retval = exec("is_dir", "/tmp");
+    ASSERT_TRUE(retval.toBool());
 }
---
+++ tests/src/hash.cpp
@@ -0,0 +1,13 @@
+#include "phpx_test.h"
+
+using namespace php;
+
+TEST(hash, md5) {
+    constexpr int l = 1024;
+    auto rdata = exec("random_byets", l);
+    ASSERT_EQ(rdata.length(), l);
+    String str(rdata);
+    String hash1 = md5(str);
+    auto hash2 = exec("md5", rdata);
+    ASSERT_STREQ(hash1.c_str(), hash2.toCString());
+}
