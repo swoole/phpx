3beb558f4ebed4660f0795fd98b193a350f924f6
matyhtf <mikan.tenny@gmail.com>
2021-08-06 16:26:01 +0800

    Optimize tests[2]

--- tests/src/exec.cpp
+++ tests/src/exec.cpp
@@ -6,3 +6,16 @@ TEST(exec, func) {
     auto retval = exec("is_dir", "/tmp");
     ASSERT_TRUE(retval.toBool());
 }
+
+TEST(exec, static_method) {
+    auto retval = exec("DateTime::createFromFormat", "j-M-Y", "15-Feb-2009");
+    ASSERT_TRUE(retval.isObject());
+    Object o(retval);
+    auto _date = o.exec("format", "Y-m-d");
+    ASSERT_STREQ(_date.toCString(), "2009-02-15");
+}
+
+TEST(exec, func_with_namespace) {
+    auto retval = exec("\\is_dir", "/tmp");
+    ASSERT_TRUE(retval.toBool());
+}
