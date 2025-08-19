f4d124556af0b1ab860b1b8dd63784e56d80c6d7
tianfenghan <rango@swoole.com>
2025-08-19 21:47:04 +0800

    Remove console

--- console/create_bin.sh
+++
@@ -1,5 +0,0 @@
-rm -rf nego/
-php console.php create nego --bin
-cd nego
-php ../console.php build -d
-
--- console/create_ext.sh
+++
@@ -1,5 +0,0 @@
-rm -rf meego/
-php console.php create meego --ext
-cd meego
-php ../console.php build -d
-
