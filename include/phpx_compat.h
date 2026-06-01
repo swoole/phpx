d2d01e38a28e57c8d7476a34d33be196ca13137a
rango <rango@swoole.com>
2026-06-01 11:57:46 +0800

    fix(build): 修复Windows平台链接库配置和宏定义冲突
    - 修正了Windows平台的链接库配置，将数学库依赖正确添加到PRIVATE目标
    - 移除了与系统头文件冲突的宏定义，包括IGNORE、DELETE、IPC_NOWAIT等
    - 解决了网络编程相关的MSG_*宏定义冲突问题
    - 修复了套接字关闭标志SHUT_*的宏定义冲突
    - 解决了ERROR和CAL_GREGORIAN宏定义的命名冲突问题

--- include/phpx_compat.h
+++ include/phpx_compat.h
@@ -32,4 +32,56 @@
 #undef ftruncate
 #endif
 
+#ifdef IGNORE
+#undef IGNORE
+#endif
+
+#ifdef DELETE
+#undef DELETE
+#endif
+
+#ifdef IPC_NOWAIT
+#undef IPC_NOWAIT
+#endif
+
+#ifdef getuid
+#undef getuid
+#endif
+
+#ifdef MSG_OOB
+#undef MSG_OOB
+#endif
+
+#ifdef MSG_WAITALL
+#undef MSG_WAITALL
+#endif
+
+#ifdef MSG_PEEK
+#undef MSG_PEEK
+#endif
+
+#ifdef MSG_DONTWAIT
+#undef MSG_DONTWAIT
+#endif
+
+#ifdef SHUT_RDWR
+#undef SHUT_RDWR
+#endif
+
+#ifdef SHUT_RD
+#undef SHUT_RD
+#endif
+
+#ifdef SHUT_WR
+#undef SHUT_WR
+#endif
+
+#ifdef ERROR
+#undef ERROR
+#endif
+
+#ifdef CAL_GREGORIAN
+#undef CAL_GREGORIAN
+#endif
+
 #endif
